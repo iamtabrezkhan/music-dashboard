ac9c66e6a9a4e7261eb75b0daf3409fa62cca143
Tabrez Khan <khan.t.29@gmail.com>
2022-09-23 00:44:42 +0530

    update: fetch music videos data on mount

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./Main.module.css";
 import PageLoader from "../PageLoader";
+import { getMusicData } from "../../http";
 
 const Main = () => {
-  return (
-    <div className={classes.main}>
-      <PageLoader />
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // fetch music data on mount
+    fetchMusicData();
+  }, []);
+
+  const fetchMusicData = async () => {
+    try {
+      const res = await getMusicData();
+      const data = res.data || {};
+    } catch (error) {
+      setError("Oops! Something went wrong!");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) {
+    return <PageLoader />;
+  }
+
+  return <div className={classes.main}></div>;
 };
 
 export default Main;
